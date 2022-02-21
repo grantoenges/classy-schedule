545cd894248a886668213afdb96300dfb2a4f4d8
Cole Potter <decerez@gmail.com>
2022-02-21 16:50:17 -0600

    Started adding fake data for web app.

---
+++ web/app/src/FakeData.js
@@ -0,0 +1,25 @@
+module.exports = {
+    teachers: {
+        1: {
+            name: "Dr. Robbin"
+        },
+        2: {
+            name: "Ed Hardy"
+        }
+    },
+
+    classrooms: {
+        100: {
+            cap: 10,
+        },
+
+        101: {
+            cap: 5,
+        },
+
+    },
+
+    classes: {
+        
+    }
+}
